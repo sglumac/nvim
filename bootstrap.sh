4e3ba77670e58248ca6bb6b6081e71817657a8e4
Slaven Glumac <slaven.glumac@gmail.com>
2025-05-20 14:01:28 +0200

    bootstrap update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,6 @@
-stow -t ~ nvim
+if ! command -v stow &> /dev/null; then
+  echo "GNU Stow is not installed! Please install it using your package manager (sudo apt install stow # for Debian/Ubuntu)."
+else
+    stow -t ~ nvim
+fi
 
